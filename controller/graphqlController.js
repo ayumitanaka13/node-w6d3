6fa214a9a5287f1b4309360e7f6c08ec6be64095
ayumitanaka <tanakayumi13@gmail.com>
2021-06-09 17:20:19 +0000

    modified graphqlController

--- controller/graphqlController.js
+++ controller/graphqlController.js
@@ -5,16 +5,28 @@ const employeeService = require("../service/employeesData");
 const schema = buildSchema(`
     type Query {
         employees: [Employee]
+        employee(id: Int!): Employee
+    }
+    type Mutation {
+        createEmployee(name: String!, department: String!): Employee
+        deleteEmployee(id: Int!): DeleteEmployeeResponse
+    }
+    type DeleteEmployeeResponse {
+        id: Int!
     }
     type Employee {
         id: Int!
-        name:String!
-        department:String!
+        name: String!
+        department: String!
     }
 `);
 
 const root = {
   employees: employeeService.getAll(),
+  employee: (graphqlInput) =>
+    employeeService.getById(graphqlInput && graphqlInput.id),
+  createEmployee: (graphqlInput) => employeeService.save(graphqlInput),
+  deleteEmployee: (graphqlInput) => employeeService.deleteById(graphqlInput.id),
 };
 
 const graphql = graphqlHTTP({
